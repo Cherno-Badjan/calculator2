e9048395f8a61b21b299c61567a079bf8f8dfb8b
Cherno Badjan <badjancherno@gmail.com>
2021-01-08 14:04:52 -0800

    cleaner tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,10 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { getSum, getDifference } from '../utils.js';
+import { getSum, getDifference, getProduct, getDiv } from '../utils.js';
 
 const test = QUnit.test;
 
-test('For the love of god please return 100 when given 50 and 50', (expect) => {
+test('For the love of god please return the correct sum when given two numbers', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = 100;
@@ -16,13 +16,58 @@ test('For the love of god please return 100 when given 50 and 50', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+
+    const expected2 = 200;
+
+
+    const actual2 = getSum(100, 100);
+
+
+    expect.equal(actual2, expected2);
+
 });
 
-test('Do not fail me not when given 50 and 25.Just return 25', (expect) => {
+
+test('For the love of god please return the correct difference when given two numbers', (expect) => {
 
     const expected = 25;
 
     const actual = getDifference(50, 25);
 
     expect.equal(actual, expected);
+
+    const expected2 = 10;
+
+    const actual2 = getDifference(50, 40);
+
+    expect.equal(actual2, expected2);
+
+});
+test('For the love of god please return the correct product when given two numbers', (expect) => {
+
+    const expected = 5000;
+
+    const actual = getProduct(50, 100);
+
+    expect.equal(actual, expected);
+
+    const expected2 = 64;
+
+    const actual2 = getProduct(8, 8);
+
+    expect.equal(actual2, expected2);
+});
+test('For the love of god please return the correct quotient when given two numbers', (expect) => {
+
+    const expected = 10;
+
+    const actual = getDiv(20, 2);
+
+    expect.equal(actual, expected);
+
+    const expected2 = 100;
+
+    const actual2 = getDiv(200, 2);
+
+    expect.equal(actual2, expected2);
 });
